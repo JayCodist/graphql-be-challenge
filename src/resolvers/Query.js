640b7623741395675f1895fa069dce3a244ec875
Johnson Ubaezuonu <jaycodist@gmail.com>
2019-11-29 23:58:41 +0100

    implemented calculatePrice query resolver -Johnson

---
+++ src/resolvers/Query.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+
+const info = () => 'Welcome to the GraphQL API for calculating the current price of BitCoin in NGN. Check the docs for usage info'
+
+
+const calculatePrice = async (parent, args, context, info) =>
+{
+	const { type, margin, exchangeRate } = args;
+	let bitCoinValue;
+	const apiResponse = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+		.then(data => data.json())
+		.then(json => bitCoinValue = parseFloat(json.bpi.USD.rate_float));
+	if (apiResponse)
+	{
+		const appliedMargin = bitCoinValue * margin / 100;
+		const adjustedValue = type === "SELL" ? (bitCoinValue - appliedMargin) : (bitCoinValue + appliedMargin);
+		console.log(exchangeRate)
+		return adjustedValue * exchangeRate;
+	}
+	 // Coindesk's API service is not always online
+}
+
+module.exports = {info, calculatePrice}
